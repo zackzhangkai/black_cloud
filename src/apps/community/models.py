3b4536ec7c709b7746d3df50ca6e3e9e83d65cba
zhuxiaoyang <sunzhu@yunify.com>
2019-09-20 19:16:51 +0800

    update community controller
    Signed-off-by: zhuxiaoyang <sunzhu@yunify.com>

--- src/apps/community/models.py
+++ src/apps/community/models.py
@@ -3,24 +3,27 @@ from django.contrib.auth.models import User
 
 
 class Topic(models.Model):
+    """社区话题表"""
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
     article = models.TextField(null=False)
     title = models.CharField(max_length=80, null=False)
-    star = models.IntegerField()
+    star = models.IntegerField(default=0)
 
 
 class Comment(models.Model):
+    """话题评论表"""
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
     comment = models.TextField(null=False)
-    star = models.IntegerField()
-    article_id = models.ForeignKey(to=Topic, on_delete=models.CASCADE)
+    star = models.IntegerField(default=0)
+    topic_id = models.ForeignKey(to=Topic, on_delete=models.CASCADE)
 
 
 class StarUser(models.Model):
+    """用户点赞关系表"""
     id = models.AutoField(primary_key=True)
     user_id = models.IntegerField()
     community_id = models.IntegerField()
