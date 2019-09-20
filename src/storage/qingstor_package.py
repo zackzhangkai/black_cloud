9f24488a5386ce3b12b3512b77c90c7191e80818
wangmq <mengqiaowang@yunify.com>
2019-09-20 23:29:14 +0800

    get url

--- src/storage/qingstor_package.py
+++ src/storage/qingstor_package.py
@@ -41,39 +41,14 @@ def qs_list():
     output = bucket.list_objects()
     return output
 
-
-def gen_view_url(qy_access_key_id='NEHJOMFLKPTUSVMXFGRN',
-                 secret_access_key='roz8ErW0q0iV0ku9s5vZnQEvSv3DuH0RiirCPg8r',
-                 object_name=''):
-    # qy_access_key_id = "NOVKGXOSNCUTIWNVIMMQ"
-    # secret_access_key = "PYXp8uBp9K5l0H2t6dTy1yRQoKF0Dtpqeq2NVknc"
-
-    verb = "GET"
-    md5 = ""
-    content_type = ""  # 请求头没有这个参数，保留空白行
-    expire_second = 300  # 设置五分钟过期
+def gen_view_url(object_name=''):
+    bucket = new_bucket()
+    expire_second = 300
     now = int(time.time())
-    expire = str(now + expire_second)
-    resource = "/hello1234/" + object_name  # testes.pek3b.qingstor.com 是 virtual-host 风格，所以 Canonicalized Resource 是 /bucketname 开头的
-    print('im res %s' % resource)
-    # host = "https://testes.pek3b.qingstor.com/1.jpg"
-    host = "https://hello1234.pek3b.qingstor.com/" + object_name
-
-    string_to_sign = verb + "\n" + md5 + "\n" + content_type + "\n" + expire + "\n" + resource
-    # h = hmac.new(secret_access_key, digestmod=sha256)
-    # h.update(string_to_sign)
-
-    # python3
-    h = hmac.new(secret_access_key.encode("utf-8"), digestmod=sha256)
-    h.update(string_to_sign.encode("utf-8"))
-
-    # signature = urllib.quote(base64.b64encode(h.digest()).strip())
-    signature = urllib.request.quote(base64.b64encode(h.digest()).strip())
-
-    uri = "?expires=" + expire + "&signature=" + signature + "&access_key_id=" + qy_access_key_id
-
-    print(host + uri)
-    return host + uri
+    prepared = bucket.get_object_request(object_name).sign_query(
+        now + expire_second
+    )
+    return prepared.url
 
 
 
