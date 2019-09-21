78ca3dfe2cb0f772632dbd8ac3d56d5ecc0e81ff
zackzhang <zackzhang@yunify.com>
2019-09-21 10:35:16 +0800

    support comment

--- community_app/views.py
+++ community_app/views.py
@@ -4,20 +4,6 @@ from django.contrib.auth.decorators import login_required
 from community_app.models import Topic, Comment
 from community_app import models
 
-# Create your views here.
-
-#
-# @login_required
-# def show_topics(request):
-#     """展示所有主题"""
-#     t_objs = Topic.objects.all()
-#     return render(request, 'community_app/topic_list.html',
-#                   context={
-#                       't_objs': t_objs
-#                   })
-
-
-
 @login_required
 def show_topics(request):
     """展示所有主题"""
@@ -37,6 +23,28 @@ def show_topics(request):
                   context={
                       't_objs': t_objs
                   })
+
+@login_required
+def show_comments(request):
+    """展示所有主题"""
+    if request.method == 'POST':
+        user = request.user
+        title = request.POST.get("title")
+        article = request.POST.get("article")
+        models.Topic.objects.create(user=user, title=title, article=article)
+        t_objs = models.Topic.objects.all()
+        return render(request, 'community_app/show_comments.html',
+                      context={
+                          't_objs': t_objs
+                      }
+        )
+    else:
+        t_objs = Topic.objects.all()
+        return render(request, 'community_app/show_comments.html',
+                  context={
+                      't_objs': t_objs
+                  }
+        )
 
 
 
